b44cd3b705c0d3ef5770eb74e633b388df860216
deepsidh <dsd@nordetect.com>
2023-06-17 13:31:13 +0200

    Zoom and pixel placement now work correctly

--- roskilde-pixel-art-frontend/src/app/components/main-canvas/main-canvas.component.ts
+++ roskilde-pixel-art-frontend/src/app/components/main-canvas/main-canvas.component.ts
@@ -12,41 +12,41 @@ export class MainCanvasComponent implements OnInit {
   selectedPixelColor: '#1aa8cb';
   isDrawing = false;
   totalPixels = 0; // get this from cookie
+  initialTouchDistance: number = 0;
+  initialPixelSize: number;
+  selectedCoordinates:any;
   constructor(private websocketService: WebsocketService) { }
   @ViewChild('canvas', { static: true })
-  canvas: ElementRef<HTMLCanvasElement>;  
-  
+  canvas: ElementRef<HTMLCanvasElement>;
+
   private context: CanvasRenderingContext2D | null = null;
 
   ngOnInit(): void {
     this.websocketService.connect();
     console.log('Connected to websocket')
     this.websocketService.onMessage().subscribe((response) => {
-       console.log('onMessage response', response);
+      console.log('onMessage response', response);
     });
     this.websocketService.onDraw().subscribe((response: any) => {
       console.log('onDraw response', response);
       this.draw(response.x, response.y, response.color, false, false);
-   });
+    });
   }
 
   ngAfterViewInit() {
-    const canvasElement = this.canvas.nativeElement 
+    const canvasElement = this.canvas.nativeElement
     this.context = canvasElement.getContext('2d');
     console.log('this.context', this.context)
-
-    // canvasElement.addEventListener('click', this.handleCanvasClick.bind(this));
 
     this.gridSize = 50;
     this.pixelSize = canvasElement.width / this.gridSize;
 
-
     canvasElement.addEventListener('mousedown', this.handleMouseDown.bind(this));
     canvasElement.addEventListener('mousemove', this.handleMouseMove.bind(this));
     canvasElement.addEventListener('mouseup', this.handleMouseUp.bind(this));
-    canvasElement.addEventListener('touchstart', this.handleTouchStart.bind(this));
-    canvasElement.addEventListener('touchmove', this.handleTouchMove.bind(this));
-    canvasElement.addEventListener('touchend', this.handleTouchEnd.bind(this));
+    canvasElement.addEventListener('pointerdown', this.handlePointerDown.bind(this));
+    canvasElement.addEventListener('pointermove', this.handlePointerMove.bind(this));
+    canvasElement.addEventListener('pointerup', this.handlePointerUp.bind(this));
 
   }
 
@@ -56,73 +56,99 @@ export class MainCanvasComponent implements OnInit {
     this.websocketService.sendMessage(event, messageObject);
   }
 
-  handleCanvasClick(event: MouseEvent): void {
-    const canvasElement = this.canvas.nativeElement;
-    const rect = canvasElement.getBoundingClientRect();
-
-    const x = Math.floor((event.clientX - rect.left) / this.pixelSize);
-    const y = Math.floor((event.clientY - rect.top) / this.pixelSize);
-    this.drawPixel(x, y, this.selectedPixelColor, true); 
-  }
-
   handleMouseDown(event: MouseEvent): void {
     this.isDrawing = true;
     this.draw(event.clientX, event.clientY);
   }
-  
+
   handleMouseMove(event: MouseEvent): void {
     if (this.isDrawing) {
       this.draw(event.clientX, event.clientY);
     }
   }
-  
+
   handleMouseUp(): void {
     this.isDrawing = false;
   }
-  
-  handleTouchStart(event: TouchEvent): void {
-    this.isDrawing = true;
-    const touch = event.touches[0];
-    this.draw(touch.clientX, touch.clientY);
+
+  handlePointerDown(event: PointerEvent): void {
+    // this.isDrawing = true;
+    if (event.pointerType === 'touch') {
+      if (event.isPrimary && event.pointerId === 1) {
+        const touch1 = event;
+        const touch2 = event.getCoalescedEvents()[0];
+        const dx = touch1.clientX - touch2.clientX;
+        const dy = touch1.clientY - touch2.clientY;
+
+        this.initialTouchDistance = Math.sqrt(dx * dx + dy * dy);
+        this.initialPixelSize = this.pixelSize;
+      } else if (event.isPrimary && event.pointerId === 0) {
+        const touch = event;
+        this.draw(touch.clientX, touch.clientY);
+      }
+    }
   }
-  
-  handleTouchMove(event: TouchEvent): void {
-    if (this.isDrawing) {
-      event.preventDefault();
-      const touch = event.touches[0];
+
+  handlePointerMove(event: PointerEvent): void {
+    // if (this.isDrawing) { //uncomment these for the tracing functionality - MIGHT NOT WORK BNOW WITH POINTER EVENTS
+    // event.preventDefault();
+    if (event.pointerType === 'touch' && event.isPrimary && event.pointerId === 1) {
+      const touch1 = event;
+      const touch2 = event.getCoalescedEvents()[0];
+      const dx = touch1.clientX - touch2.clientX;
+      const dy = touch1.clientY - touch2.clientY;
+      const currentTouchDistance = Math.sqrt(dx * dx + dy * dy);
+
+      const scaleFactor = currentTouchDistance / this.initialTouchDistance;
+      this.pixelSize = this.initialPixelSize * scaleFactor;
+
+      const canvasElement = this.canvas.nativeElement;
+      const rect = canvasElement.getBoundingClientRect();
+      const canvasCenterX = (rect.width / 2 - rect.left) / this.pixelSize;
+      const canvasCenterY = (rect.height / 2 - rect.top) / this.pixelSize;
+
+      const scaledCanvasWidth = rect.width / this.pixelSize;
+      const scaledCanvasHeight = rect.height / this.pixelSize;
+      const deltaX = (canvasCenterX - scaledCanvasWidth / 2) * (1 - scaleFactor);
+      const deltaY = (canvasCenterY - scaledCanvasHeight / 2) * (1 - scaleFactor);
+
+      const left = rect.left - deltaX * this.pixelSize;
+      const top = rect.top - deltaY * this.pixelSize;
+
+      canvasElement.style.left = `${left}px`;
+      canvasElement.style.top = `${top}px`;
+    } else if (event.pointerType === 'touch' && event.isPrimary && event.pointerId === 0) {
+      const touch = event;
       this.draw(touch.clientX, touch.clientY);
     }
   }
-  
-  handleTouchEnd(): void {
+  // }
+
+  handlePointerUp(): void {
     this.isDrawing = false;
   }
-  drawPixel(x: number, y: number, color: string, emit: boolean): void {
-    console.log('drawPixel',x,y,color);
-    this.context.fillStyle = color;
-    this.context.fillRect(x * this.pixelSize, y * this.pixelSize, this.pixelSize, this.pixelSize);
-    if (emit){
-      this.sendMessage('draw', {x: x, y: y, color: color});
-    }
-  }
 
-  draw(x: number, y: number, color: string = this.selectedPixelColor, emit: boolean = true, isOwner : boolean = true): void {
-    
+  draw(x: number, y: number, color: string = this.selectedPixelColor, emit: boolean = true, isOwner: boolean = true): void {
     const canvasElement = this.canvas.nativeElement;
     const rect = canvasElement.getBoundingClientRect();
 
-    let canvasX = Math.floor((x - rect.left) / this.pixelSize);
-    let canvasY = Math.floor((y - rect.top) / this.pixelSize);
+    const canvasX = Math.floor((x - rect.left) / this.pixelSize);
+    const canvasY = Math.floor((y - rect.top) / this.pixelSize);
 
-    canvasX = x - rect.left;
-    canvasY = y - rect.top;
-    
     this.context.fillStyle = color;
-    this.context.fillRect(canvasX, canvasY, this.pixelSize, this.pixelSize);
-    if (emit){
-      this.sendMessage('draw', {x: x, y: y, color: color});
+    this.context.fillRect(
+      canvasX * this.pixelSize,
+      canvasY * this.pixelSize,
+      this.pixelSize,
+      this.pixelSize
+    );
+
+    if (emit) {
+      this.selectedCoordinates = "X:"+x + ",y:"+y;
+      this.sendMessage('draw', { x: x, y: y, color: color });
     }
-    if(isOwner) {
+
+    if (isOwner) {
       this.totalPixels += 1;
     }
   }
