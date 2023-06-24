ae947e44ee60e8d6d32ef24bc702be4680077e86
Javi <vatojavier@gmail.com>
2023-06-24 18:56:15 +0200

    added enpoints

--- data/scripts/init.sql
+++ data/scripts/init.sql
@@ -10,23 +10,24 @@ GRANT ALL PRIVILEGES ON SCHEMA public TO python;
 -- Tables that are created by the application
 SET ROLE python;
 
-CREATE TABLE users (
-    user_id UUID PRIMARY KEY,
-    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    last_seen_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    pixels_left INT NOT NULL,
-    last_pixel_placed_at TIMESTAMP WITH TIME ZONE
-);
-
 CREATE TABLE canvas (
   id SERIAL PRIMARY KEY,
   color INT
 );
 
+CREATE TABLE users (
+    user_id UUID PRIMARY KEY,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    last_seen_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    pixels_left INT NOT NULL,
+    reset_pixel_placed_at TIMESTAMP WITH TIME ZONE
+);
+
+
 CREATE TABLE canvas_history (
     id SERIAL PRIMARY KEY,
-    user_id UUID NOT NULL REFERENCES users(user_id),
-    tile_id INT NOT NULL REFERENCES canvas(id),
+    user_id UUID NOT NULL,
+    tile_id INT NOT NULL,
     color INT NOT NULL,
     placed_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
