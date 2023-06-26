436749f67335f0253768600da7b4bf700a03e7fe
deepsidh <dsd@nordetect.com>
2023-06-26 16:09:38 +0200

    Fixed timer bug for new user

--- roskilde-pixel-art-frontend/src/app/components/main-canvas/main-canvas.component.ts
+++ roskilde-pixel-art-frontend/src/app/components/main-canvas/main-canvas.component.ts
@@ -53,7 +53,7 @@ export class MainCanvasComponent implements OnInit {
   deleteMode: boolean = false;
   selectedPixels: any[];
   password: '';
-  serverURL: 'http://roskildepixel.dk/'
+  serverURL: 'https://roskildepixel.dk/'
   timer: any;
   defaultTimeout: number = 5;
   correctPassword: any = null;
@@ -81,15 +81,15 @@ export class MainCanvasComponent implements OnInit {
     this.makeInitialDisplayTime()
     // this.remainingTime = 10;
     // Fetch msg from backend.
-    // this.http.get('http://roskildepixel.dk/api/get_msg').subscribe((data: any) => {
+    // this.http.get('https://roskildepixel.dk/api/get_msg').subscribe((data: any) => {
     //   this.msg = data.message;
     //   console.log(this.msg);
     // });
 
     // Set cookie and send it with the request
-    console.log(('http://roskildepixel.dk/'+'api/get_cookie'))
+    console.log(('https://roskildepixel.dk/'+'api/get_cookie'))
 
-    this.http.get('http://roskildepixel.dk/'+'api/get_cookie', { withCredentials: true }).subscribe((data: any) => {
+    this.http.get('https://roskildepixel.dk/'+'api/get_cookie', { withCredentials: true }).subscribe((data: any) => {
       console.log('Cookie:', data);
       this.userID = data.user_id;
       this.isFirstTimeUser = data.is_first_time_user;
@@ -98,32 +98,36 @@ export class MainCanvasComponent implements OnInit {
 
 
 
-      this.http.get('http://roskildepixel.dk/'+'api/get_max_pixels_per_user', { withCredentials: true }).subscribe((data: any) => {
+      this.http.get('https://roskildepixel.dk/'+'api/get_max_pixels_per_user', { withCredentials: true }).subscribe((data: any) => {
         console.log('Max pixels:', data);
         this.totalAllowedPixels = data.max_pixels_per_user
+        this.makeInitialDisplayTime()
+
       });
 
-      this.http.get('http://roskildepixel.dk/'+'api/get_pixels_left', { withCredentials: true }).subscribe((data: any) => {
+      this.http.get('https://roskildepixel.dk/'+'api/get_pixels_left', { withCredentials: true }).subscribe((data: any) => {
         console.log('Pixels left:', data);
         this.pixelsLeft = data.pixels_left
-
+        this.makeInitialDisplayTime()
       });
 
-      this.http.get('http://roskildepixel.dk/'+'api/get_max_cool_down_time', { withCredentials: true }).subscribe((data: any) => {
+      this.http.get('https://roskildepixel.dk/'+'api/get_max_cool_down_time', { withCredentials: true }).subscribe((data: any) => {
         console.log('max_cool_down_seconds:', data);
         this.defaultTimeout = data.max_cool_down_seconds
         this.remainingTime = data.max_cool_down_seconds
 
-        this.http.get('http://roskildepixel.dk/' + 'api/get_cool_down_time_left', { withCredentials: true }).subscribe((data: any) => {
+        this.http.get('https://roskildepixel.dk/' + 'api/get_cool_down_time_left', { withCredentials: true }).subscribe((data: any) => {
           console.log('cool_down_time_left:', data);
           if (data.cool_down_time_left) {
             this.remainingTime = data.cool_down_time_left
             this.startTimer()
           }
+          this.makeInitialDisplayTime()
+
         })
       });
 
-      this.http.get('http://roskildepixel.dk/'+'api/get_cool_down_time_left', { withCredentials: true }).subscribe((data: any) => {
+      this.http.get('https://roskildepixel.dk/'+'api/get_cool_down_time_left', { withCredentials: true }).subscribe((data: any) => {
         console.log('cool_down_time_left:', data);
       });
 
@@ -269,7 +273,7 @@ export class MainCanvasComponent implements OnInit {
     footer.style.transform = `scale(${window.innerWidth / document.documentElement.clientWidth})`;
   }
   fetchCanvasData(): void {
-    this.http.get<any[]>('http://roskildepixel.dk/'+'api/get_canvas').subscribe(
+    this.http.get<any[]>('https://roskildepixel.dk/'+'api/get_canvas').subscribe(
       (data: any[]) => {
 
         this.canvasData = data;
@@ -429,7 +433,7 @@ export class MainCanvasComponent implements OnInit {
   //   console.log('draw', x, y);
   checkPassword() {
     let  response = false;
-    this.http.post<any>('http://roskildepixel.dk/' + 'api/check_password', {password: this.password}).subscribe(data => {
+    this.http.post<any>('https://roskildepixel.dk/' + 'api/check_password', {password: this.password}).subscribe(data => {
       console.log('check password', data);
       if (data.message === this.password){
         this.correctPassword = this.password;
@@ -509,7 +513,7 @@ export class MainCanvasComponent implements OnInit {
     
     if (isOwner) {
       this.pixelsLeft -= 1;
-      if (this.pixelsLeft === 1) {
+      if (this.pixelsLeft === this.totalAllowedPixels-1) {
         this.startTimer();
       }
     }
@@ -550,7 +554,7 @@ export class MainCanvasComponent implements OnInit {
     console.log('pixelIds', pixelIds)
     this.selectedPixels = [];
     const data = { password: this.password, pixel_ids: pixelIds }
-    this.http.post<any>('http://roskildepixel.dk/'+'api/delete_pixels', data).subscribe(data => {
+    this.http.post<any>('https://roskildepixel.dk/'+'api/delete_pixels', data).subscribe(data => {
       console.log('delete', data);
     }, error => {
       console.log('delete error', error)
